92810302568602a8b727086375f240f2d8467099
Glenn Washburn <development@efficientek.com>
2019-07-05 19:07:04 -0500

    Initial miracast enabler module

--- install.sh
+++ install.sh
@@ -122,9 +122,9 @@ REPLACE="
 # Set what you want to display when installing your module
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "********************************************"
+  ui_print "     Miracast Enabler for Pixel * on Pie    "
+  ui_print "********************************************"
 }
 
 # Copy/extract your module files into $MODPATH in on_install.
